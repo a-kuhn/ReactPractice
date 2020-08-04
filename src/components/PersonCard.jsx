a3026c091d0ce2ad6d2b9d6ff1671ad22a4e7d48
alex k <alex.kuhn1123@gmail.com>
2020-08-04 16:18:25 -0700

    create PersonCard component

---
+++ src/components/PersonCard.jsx
@@ -0,0 +1,10 @@
+{/*
+Create a component called PersonCard that accepts the following props: 
+1. firstName
+2. lastName
+3. age
+4. hairColor
+
+Then, display four PersonCard components that show four different people, using props.
+
+*/}
