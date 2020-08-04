282833cef0cb14d8317b41a65a455b20109ebb99
alex k <alex.kuhn1123@gmail.com>
2020-08-04 16:22:22 -0700

    complete PersonCard component

--- src/components/PersonCard.jsx
+++ src/components/PersonCard.jsx
@@ -8,3 +8,21 @@ Create a component called PersonCard that accepts the following props:
 Then, display four PersonCard components that show four different people, using props.
 
 */}
+
+import React, {Component} from 'react';
+
+class PersonCard extends Component {
+    render() {
+        // destructure props passed down:
+        const {firstName, lastName, age, hairColor } = this.props;
+        return (
+            <div>
+                <h1>{lastName}, {firstName}</h1>
+                <h4>Age: {age}</h4>
+                <h4>Hair Color: {hairColor}</h4>
+            </div>
+        );
+    }
+}
+
+export default PersonCard;
