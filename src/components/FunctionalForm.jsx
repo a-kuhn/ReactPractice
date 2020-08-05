79a2b2de98b5d1ff95a574b821aa888ba79ba41d
alex k <alex.kuhn1123@gmail.com>
2020-08-05 16:31:56 -0700

    add real-time display of form input

--- src/components/FunctionalForm.jsx
+++ src/components/FunctionalForm.jsx
@@ -30,30 +30,39 @@ export default () => {
     }
 
     return(
-        //call createLocation when submit button clicked
-        <form onSubmit={ createLocation }>
-            <div>
-                <label>Address: </label>
-                {/* 
-                onChange takes callback. e==event: holds lots of info. we want target's (this input box) value (the stuff typed into this box in browser)  
-                
-                value={address} ==> creates 2-way binding. whatever is in address (state) is displayed here
-                */}
-                <input type="text" onChange={(e)=>setAddress(e.target.value)} value={address}/>
-            </div>
-            <div>
-                <label>City: </label>
-                <input type="text" onChange={(e)=>setCity(e.target.value)} value={city}/>
-            </div>
-            <div>
-                <label>County: </label>
-                <input type="text" onChange={(e)=>setCounty(e.target.value)} value={county}/>
-            </div>
-            <div>
-                <label>State: </label>
-                <input type="text" onChange={(e)=>setState(e.target.value)} value={state}/>
-            </div>
-            <button type="submit">SUBMIT</button>
-        </form>
+        //enclose everything in a single div!
+        <div>
+            {/* call createLocation when submit button clicked */}
+            <form onSubmit={ createLocation }>
+                <div>
+                    <label>Address: </label>
+                    {/* 
+                    onChange takes callback. e==event: holds lots of info. we want target's (this input box) value (the stuff typed into this box in browser)  
+                    
+                    value={address} ==> creates 2-way binding. whatever is in address (state) is displayed here
+                    */}
+                    <input type="text" onChange={(e)=>setAddress(e.target.value)} value={address}/>
+                </div>
+                <div>
+                    <label>City: </label>
+                    <input type="text" onChange={(e)=>setCity(e.target.value)} value={city}/>
+                </div>
+                <div>
+                    <label>County: </label>
+                    <input type="text" onChange={(e)=>setCounty(e.target.value)} value={county}/>
+                </div>
+                <div>
+                    <label>State: </label>
+                    <input type="text" onChange={(e)=>setState(e.target.value)} value={state}/>
+                </div>
+                <button type="submit">SUBMIT</button>
+            </form>
+
+            <h5>real-time display of form data: </h5>
+            <p>Address: {address}</p>
+            <p>City: {city}</p>
+            <p>County: {county}</p>
+            <p>State: {state}</p>
+        </div>
     );
 }
