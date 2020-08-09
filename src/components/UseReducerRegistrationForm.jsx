ca0c4efc77229ce592d89fc217baaad3a340937f
alex k <alex.kuhn1123@gmail.com>
2020-08-08 18:02:01 -0700

    still trying to get this component to work

--- src/components/UseReducerRegistrationForm.jsx
+++ src/components/UseReducerRegistrationForm.jsx
@@ -32,7 +32,7 @@ const reducer = (state, action) => {
 
 export default () => {
     const [state, dispatch] = useReducer(reducer, initialState);
-
+    // console.log(`state: ${state.firstName.value}`);
     const onChangeHandler = (e) => {
         console.log('********** onChangeHandler triggered...');
         const {name, value} = e.target;
@@ -40,25 +40,30 @@ export default () => {
             type: name,
             payload: value
         });
+        if (name == 'firstName' && value.length<2){
+            dispatch({
+                type: name,
+                error: "First name required"
+        });}
     }
 
     const handleFirstName = (e) => {
         console.log(`first name length: ${e.target.value.length}`);
         if(e.target.value.length < 1 ){
-            console.log(`first name not long enough...setting initialState.firstName.error`);
-            initialState.firstName.error = ("First name is required!");
+            console.log(`first name not long enough...setting state.firstName.error`);
+            state.firstName.error = ("First name is required!");
         }else if(e.target.value.length < 2){
-            initialState.firstName.error = ("First name must be at least 2 characters long!");
-        }else {initialState.firstName.error = null;}
+            state.firstName.error = ("First name must be at least 2 characters long!");
+        }else {state.firstName.error = null;}
         onChangeHandler(e);
     }
     const handleLastName = (e) => {
         onChangeHandler(e);
         if(e.target.value.length < 1 ){
-            initialState.lastName.error = ("Last name is required!");
+            state.lastName.error = ("Last name is required!");
         }else if(e.target.value.length < 2){
-            initialState.lastName.error = ("Last name must be at least 2 characters long!");
-        }else {initialState.lastName.error = null;}
+            state.lastName.error = ("Last name must be at least 2 characters long!");
+        }else {state.lastName.error = null;}
     }
     const handleEmail = (e) => {
         let emailRegEx = /\S+@\S+\.\S+/;
@@ -66,35 +71,35 @@ export default () => {
         const emailIsValid = (potentialEmail) => emailRegEx.test(potentialEmail);
         console.log(`emailIsValid: ${emailIsValid(potentialEmail)}`);
         if(potentialEmail.length < 1 ){
-            initialState.email.error = ("Email is required!");
+            state.email.error = ("Email is required!");
         }else if(potentialEmail.length < 5){
-            initialState.email.error = ("Email must be at least 5 characters long!");
+            state.email.error = ("Email must be at least 5 characters long!");
         }else if(!emailIsValid(potentialEmail)){
             console.log('invalid email detected...');
-            initialState.email.error = ("Must be a valid email address!");
-        }else {initialState.email.error = null;}
+            state.email.error = ("Must be a valid email address!");
+        }else {state.email.error = null;}
         onChangeHandler(e);
     }
     const handlePw = (e) => {
         onChangeHandler(e);
         if(e.target.value.length < 1 ){
-            initialState.pw.error = ("Password is required!");
+            state.pw.error = ("Password is required!");
         }else if(e.target.value.length < 8){
-            initialState.pw.error = ("Password must be at least 8 characters long!");
-        }else {initialState.pw.error = null;}
+            state.pw.error = ("Password must be at least 8 characters long!");
+        }else {state.pw.error = null;}
     }
     const handlePwConfirm = (e) => {
         onChangeHandler(e);
-        if(e.target.value !== initialState.pw ){
-            initialState.pwConfirm.error = ("Passwords must match!");
-        }else {initialState.pwConfirm.error = null;}
+        if(e.target.value !== state.pw ){
+            state.pwConfirm.error = ("Passwords must match!");
+        }else {state.pwConfirm.error = null;}
     }
 
     // helper method for onSubmit handler
     // when server is built out, this might include a POST request
     const onSubmitHandler = (e) => {
         e.preventDefault();
-        const {firstName, lastName, email, pw} = initialState;
+        const {firstName, lastName, email, pw} = state;
         const newUser = {firstName, lastName, email, pw};
 
         console.log(`newUser: 
@@ -107,7 +112,7 @@ export default () => {
     // build form:
     return(
         <div>
-            {/* {JSON.stringify(state)} */}
+            {JSON.stringify(state)}
             <form className="container" onSubmit={onSubmitHandler}>
                 <div className="form-group text-left">
                     {state.firstName.error != null && ( 
