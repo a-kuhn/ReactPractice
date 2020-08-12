4aeef7cf4d825f82a0e083d172cbc77fd54bc502
alex k <alex.kuhn1123@gmail.com>
2020-08-12 09:57:16 -0700

    refactor to make tabs a list of objects

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -1,26 +1,30 @@
 import React, {useState} from 'react';
 
 export default () => {
-    const items = ['Tab 1', 'Tab 2', 'Tab 3']
+    const tabs = [
+        {label: 'Tab 1', content: 'Tab 1 content showing here.'},
+        {label: 'Tab 2', content: 'Tab 2 content on DISPLAY'},
+        {label: 'Tab 3', content: 'Tab 3 content comin in hawwt'}
+    ];
 
     const [msg, setMsg] = useState('');
 
     const onClickHandler = (e, item) => {
-        setMsg(`${item} content is showing here.`);
+        setMsg(item.content);
     }
 
     return(
-        <div>
+        <div className="container">
             <div className="d-inline-flex">
-                {items.map((item, idx) => 
+                {tabs.map((item, idx) => 
                 <button 
-                    className="btn btn-lg px-4 py-2 mx-2 my-3"
+                    className="btn btn-secondary btn-lg px-4 py-2 mx-2 my-3"
                     key={idx}
                     onClick={(e) => onClickHandler(e, item)}
-                >{item}</button>
+                >{item.label}</button>
                 )}
             </div>
-            <div>
+            <div className="d-flex justify-content-around border">
                 <p>{msg}</p>
             </div>
         </div>
