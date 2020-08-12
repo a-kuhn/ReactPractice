b8999bdbb1d2567a73a512bf994f8163c7a1a549
alex k <alex.kuhn1123@gmail.com>
2020-08-12 10:27:32 -0700

    start working on ToDo List component

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,15 @@
+import React, {useState} from 'react';
+
+export default () => {
+    const [todos, setTodos] = useState([]);
+    
+    // onSubmitHandler -- add new todo to todos todo== {goal: '', isComplete: t/f}
+
+    // onDeleteHandler -- run .filter on todos, removing todo[idx]
+
+    return(
+        <div>
+
+        </div>
+    );
+}
