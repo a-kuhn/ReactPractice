a95001cd4952e564dfd5d55e0affad999e720e39
alex k <alex.kuhn1123@gmail.com>
2020-08-28 18:15:47 -0700

    create server.js file

---
+++ Faker_API/server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+const faker = require('faker');
+
+class User {
+    constructor() {
+        this.firstName = faker.name.firstName();
+        this.lastName = faker.name. lastName();
+        this.phoneNumber = faker.phone.phoneNumber();
+        this.email = faker.internet.email();
+        this.password = faker.internet.password();
+    }
+}
+
+class Company {
+    constructor() {
+        this.name = faker.company.companyName();
+        this.address = {
+            street = faker.address.streetAddress(),
+            city = faker.address.city(),
+            state = faker.address.state(),
+            zipCode = faker.address.zipCode(),
+            country = faker.address.country(),
+        };
+    }
+}
+
+app.post("/api/users/new", (req, res) => {
+    const newUser = new User();
+    res.json({user: newUser});
+});
+
+app.listen(port, ()=>console.log(`Listening on port: ${port}`));
