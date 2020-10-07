d51afc0edddf7b9968d1ee730140fa388a2cd9d9
alex k <alex.kuhn1123@gmail.com>
2020-10-06 19:19:03 -0700

    add validations to UserSchema

--- HelloMongoose/server/models/user.model.js
+++ HelloMongoose/server/models/user.model.js
@@ -1,8 +1,31 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-	name: String,
-	age: Number
+	first_name: {
+		type: String,
+		required: [true, "{PATH} is required."],
+		minlength: [2, "{PATH} must be at least 2 characters long."]
+	},
+	last_name: {
+		type: Number,
+		required: [true, "{PATH} is required."],
+		minlength: [2, "{PATH} must be at least 2 characters long."]
+	},
+	user_name: {
+		type: String,
+		required: [true, "{PATH} is required."],
+		minlength: [6, "{PATH} must be at least 6 characters long."]
+	},
+	email: {
+		type: String,
+		required: [true, "{PATH} is required."],
+		//todo: make a custom email validator
+	},
+	pw: {
+		type: String,
+		required: [true, "{PATH} is required."],
+		minlength: [8, "{PATH} must be at least 8 characters long."]
+	}
 }, {timestamps: true}); // {timestamps: true} tells mongoose to auto-manage createdAt and updatedAt fields as Date objects
 
 const User = mongoose.model("User", UserSchema);
