7c72ca0121a7a76e0c48d1724f7a2d835e01975a
alex k <alex.kuhn1123@gmail.com>
2020-10-06 19:12:00 -0700

    add timestamps to UserSchema

--- HelloMongoose/server/models/user.model.js
+++ HelloMongoose/server/models/user.model.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema({
 	name: String,
 	age: Number
-});
+}, {timestamps: true}); // {timestamps: true} tells mongoose to auto-manage createdAt and updatedAt fields as Date objects
 
 const User = mongoose.model("User", UserSchema);
 
