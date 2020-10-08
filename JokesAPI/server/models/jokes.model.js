8ffd9a1ad49725369f7937f7981ccde24d3f395f
alex k <alex.kuhn1123@gmail.com>
2020-10-07 18:28:44 -0700

    build and export JokeSchema

---
+++ JokesAPI/server/models/jokes.model.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const JokeSchema = new mongoose.Schema({
+    setup: {
+        type: String,
+		required: [true, "{PATH} is required."],
+		minlength: [10, "{PATH} must be at least 10 characters long."]
+    },
+    punchline: {
+        type: String,
+		required: [true, "{PATH} is required."],
+		minlength: [3, "{PATH} must be at least 3 characters long."]
+    }
+}, {timestamps: true});
+
+const Joke = mongoose.model("Joke", JokeSchema);
+
+module.exports = Joke;
