7f0dc5309c013d69184161d24c3e5328e816281c
alex k <alex.kuhn1123@gmail.com>
2020-10-07 18:52:15 -0700

    build out all routes to controller methods

---
+++ JokesAPI/server/routes/jokes.routes.js
@@ -0,0 +1,16 @@
+const JokesController = require('../controllers/jokes.controller');
+
+module.exports = app => {
+    //get all jokes
+    app.get("/api/jokes/", JokesController.getAllJokes);
+    //get single jokes
+    app.get("/api/jokes/:id", JokesController.getSingleJoke);
+    //get random joke
+    app.get("/api/jokes/random", JokesController.getRandomJoke);
+    //create new joke
+    app.post("/api/jokes/new", JokesController.createJoke);
+    //update existing joke
+    app.post("/api/jokes/update/:id", JokesController.updateJoke);
+    //delete single joke
+    app.delete("/api/jokes/delete/:id", JokesController.deleteJoke);
+}
