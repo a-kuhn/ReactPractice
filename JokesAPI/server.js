189f97b81cb64d589a98753c108323719cf88a79
alex k <alex.kuhn1123@gmail.com>
2020-10-07 18:23:40 -0700

    rename joke --> jokes

--- JokesAPI/server.js
+++ JokesAPI/server.js
@@ -7,7 +7,7 @@ require("./server/config/mongoose.config");
 app.use(express.json(), express.urlencoded({ extended: true }));
 
 // This is where we import the users routes function from our user.routes.js file
-const AllMyUserRoutes = require("./server/routes/joke.routes.js");
+const AllMyUserRoutes = require("./server/routes/jokes.routes.js");
 AllMyUserRoutes(app);
 
 app.listen(8000, () => console.log("The server is all fired up on port 8000"));
