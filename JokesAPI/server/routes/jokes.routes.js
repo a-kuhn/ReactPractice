c6aa74fa55345a53bbf308b07dff40f92ac65a67
alex k <alex.kuhn1123@gmail.com>
2020-10-08 21:41:30 -0700

    move /random above /:id in get request routes

--- JokesAPI/server/routes/jokes.routes.js
+++ JokesAPI/server/routes/jokes.routes.js
@@ -3,10 +3,10 @@ const JokesController = require('../controllers/jokes.controller');
 module.exports = app => {
     //get all jokes
     app.get("/api/jokes", JokesController.getAllJokes);
-    //get single jokes
-    app.get("/api/jokes/:id", JokesController.getSingleJoke);
     //get random joke
     app.get("/api/jokes/random", JokesController.getRandomJoke);
+    //get single jokes
+    app.get("/api/jokes/:id", JokesController.getSingleJoke);
     //create new joke
     app.post("/api/jokes/new", JokesController.createJoke);
     //update existing joke
