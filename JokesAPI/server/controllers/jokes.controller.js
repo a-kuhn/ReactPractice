ea7a78980c61ded260f72c3bbc6426aa906b02e3
alex k <alex.kuhn1123@gmail.com>
2020-10-08 21:41:56 -0700

    refactor getRandomJoke(); it's still broken

--- JokesAPI/server/controllers/jokes.controller.js
+++ JokesAPI/server/controllers/jokes.controller.js
@@ -17,19 +17,17 @@ module.exports = {
 
     //get a random joke
     getRandomJoke(req, res) {
+        console.log("getting a random joke...");
         Joke.find()
             .then(
-                allJokes => {
-                    const numOfJokes = length(allJokes);
-                    const randJokeNum = Math.floor(Math.random()*numOfJokes);
-                    res.json({joke: allJokes[randJokeNum]});
-                }
-            )
-            .catch(err => res.json({message: "Something went wrong.", error: err}));
-
-        const numOfJokes = length(allJokes);
-        const randJokeNum = Math.floor(Math.random() * numOfJokes);
-        
+                r => {
+                    const allJokes = r;
+                    const jokesCount = length(allJokes);
+                    const randNum = parseInt(Math.floor(Math.random()*jokesCount));
+                    const randJoke = allJokes[randNum];
+                    res.json({joke: randJoke});
+                })
+            .catch(err => res.json({ message: "Something went wrong", error: err }));
     },
 
     //create a joke
